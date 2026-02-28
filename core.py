49efb06f9e5aa04995ac3febe4eb40e2db999bdf
onmitsukido <alexeypopov1301@mail.ru>
2026-02-28 13:38:17 +1000

    parser done

---
+++ core.py
@@ -0,0 +1,36 @@
+from typing import Dict, List
+from price_fetcher import fetch_prices
+from cache_client import get_price, set_price
+
+async def get_prices_with_cache(coins: List[str], currencies: List[str]) -> Dict[str, Dict[str, str]]:
+    all_cached = True
+    result: dict[str, dict[str, str]] = {}
+
+    for coin in coins:
+        result[coin] = {}
+        for currency in currencies:
+            cached_value = await get_price(coin, currency)
+            if cached_value is None:
+                all_cached = False
+            result[coin][currency] = cached_value
+
+    if all_cached:
+        print("Все данные получены из кэша")
+        return result
+    else:
+        print("Запрашиваем свежие данные у CoinGecko...")
+
+    fresh_data: dict = await fetch_prices(coins, currencies)
+
+    for coin in coins:
+        for currency in currencies:
+            price_value = fresh_data[coin][currency]
+            await set_price(coin, currency, price_value)
+
+    final_result: dict[str, dict[str, str]] = {}
+    for coin in coins:
+        final_result[coin] = {}
+        for currency in currencies:
+            final_result[coin][currency] = str(fresh_data[coin][currency])
+
+    return final_result
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import asyncio
-from price_fetcher import fetch_prices
 import json
+from core import get_prices_with_cache
 
 async def main():
     coins = ["bitcoin", "ethereum", "solana"]
     currencies = ["usd", "rub"]
 
-    print("Запрашиваем курсы у CoinGecko...")
-    prices = await fetch_prices(coins, currencies)
+    print("Получаем курсы (с использованием кэша)...")
+    prices = await get_prices_with_cache(coins, currencies)
     
     print(json.dumps(prices, indent=2, ensure_ascii=False))
 
